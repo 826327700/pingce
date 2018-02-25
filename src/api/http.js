a916250735e8c875a9ca5ec2a7e8c9c60f492cea
xiaobaozi <826327700@qq.com>
2018-02-25 20:39:43 +0800

    更新代码

--- src/api/http.js
+++ src/api/http.js
@@ -1,19 +1,19 @@
 const http=(url='',data={})=>{
-    let baseUrl='https://www.easy-mock.com/mock/5a869945ebe94f250bc0f872/api/services/wx'//'https://域名/api/services/wx'
+    let baseUrl='https://test-api-survey.ibaizhi.com/api/services/wx'//'https://域名/api/services/wx'
     return new Promise((resolve, reject)=>{
         wx.request({
             url: baseUrl+url, 
             data: data,
             method:'POST',
             header: {
-                'Content-Type': 'json', // 默认值
+                'Content-Type': 'application/json', // 默认值
                 'Authentication':wx.getStorageSync('token')?wx.getStorageSync('token'):''
             },
             success: (res)=> {
                 resolve(res)
                 if(!res.data.success){
                     wx.showToast({
-                        title: res.data.error||"网络错误",
+                        title: res.data.error.message||"网络错误",
                         icon: 'none',
                         duration: 2000
                     })
